7b55733eabfb0f1cf0d2cea9315ed6999cd4e77c
FurryDeveloper <furrysecurity@protonmail.com>
2020-05-24 19:42:59 +1200

    add views engine!

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,26 @@ import { createConnection, Connection } from 'typeorm';
 createConnection(require(path.join(__dirname, '../ormconfig.json'))).then(connection => {
     const app = express();
 
+    app.set('view engine', 'ejs');
+
     app.use(express.json());
 
     app.use('/api/user', userRouter);
 
     app.use('/api/auth/login', loginRouter);
     app.use('/api/auth/register', registerRouter);
+
+    app.get('/', (req, res) => {
+        res.render('pages/index');
+    })
+
+    app.get('/about', (req, res) => {
+        res.render('pages/about');
+    });
+    
+    app.get('/panel/', (req, res) => {
+        res.render('pages/panel/index');
+    });
 
     app.use((res, req) => {
         req.json({ success: false, message: 'file not found on this server!' });
