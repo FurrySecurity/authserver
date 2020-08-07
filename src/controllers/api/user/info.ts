e0c353b27344f968f8e60e5f02a63b3ce970353a
FurryDeveloper <63852241+FurryDeveloper@users.noreply.github.com>
2020-08-08 00:39:52 +1200

    Update info.ts

--- src/controllers/api/user/info.ts
+++ src/controllers/api/user/info.ts
@@ -5,17 +5,19 @@ import { getManager } from 'typeorm';
 import { UserEntity } from '../../../database/entity/user'
 
 import * as moment from 'moment';
+import { ProductEntity } from '../../../database/entity/product';
 
 export class InfoController extends CrudController {
     public get(req: Request<import("express-serve-static-core").ParamsDictionary>, res: Response): void {
-        const repository = getManager().getRepository(UserEntity);
+        const userRepository = getManager().getRepository(UserEntity);
+        const productRepository = getManager().getRepository(ProductEntity);
 
         if (req.session == undefined || !req.session.authenticated) {
             res.json({ success: false, message: 'not authenticated!' });
             return;
         }
 
-        repository.findOne({ where: { id: req.session.uid } }).then(user => {
+        userRepository.findOne({ where: { id: req.session.uid } }).then(user => {
             if (user == undefined) {
                 res.json({ success: false, message: 'failed to fetch profile information!' });
             }
@@ -26,24 +28,45 @@ export class InfoController extends CrudController {
 
                 let subscriptions = JSON.parse(user.subscriptions);
 
-                console.dir(subscriptions);
-
-                for (let [key, value] of subscriptions) {
-                    console.log(value);
-
-                    if (value.expires == null || value.expires > moment.now()) {
-                        delete subscriptions[key];
+                productRepository.find().then(products => {
+                    for (let [key, value] of subscriptions) {
+                        console.log(value);
+    
+                        if (value.expires == null || value.expires > moment.now()) {
+                            delete subscriptions[key];
+                        }
                     }
-                }
 
-                res.json({ success: true, message: {
-                    id: user.id,
-                    account_type: user.account_type,
-                    email: user.email,
-                    username: user.username,
-                    subscriptions: subscriptions,
-                    credit: user.credit
-                }});
+                    for (let i = 0; i < products.length; ++i) {
+                        let product = products[i];
+                        let info = subscriptions[product.id];
+
+                        if (info == undefined) {
+                            continue;
+                        }
+
+                        info.name = product.name;
+                        info.status = product.status;
+                        info.id = product.id;
+
+                        if (info.expires == null || moment.now() > info.expires) {
+                            delete subscriptions[product.id];
+                        }
+                    }
+
+                    const result = Object.keys(subscriptions).map((index) => {
+                        return subscriptions[index];
+                    });
+    
+                    res.json({ success: true, message: {
+                        id: user.id,
+                        account_type: user.account_type,
+                        email: user.email,
+                        username: user.username,
+                        subscriptions: result,
+                        credit: user.credit
+                    }});
+                });
             }
         });
     }
